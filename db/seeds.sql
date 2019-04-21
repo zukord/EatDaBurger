f615dec59c73045cd87253701ce3acc8fbf43202
David Zukor <zukord@gmail.com>
2019-04-21 13:24:49 -0400

    Switching to laptop because it can run mysql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES (hamburger, true)
+INSERT INTO burgers (burger_name, devoured) VALUES (cheeseburger, true)
+INSERT INTO burgers (burger_name, devoured) VALUES (tofuburger, false)
