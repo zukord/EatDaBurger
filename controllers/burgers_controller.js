71a966efe3a53ceb339554c94c30046f163c650a
David Zukor <zukord@gmail.com>
2019-04-21 23:30:50 -0400

    Final GH commit before Heroku push

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,9 +18,9 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "name", "devoured"
+    "burger_name", "devoured"
   ], [
-    req.body.name, req.body.devoured
+    req.body.burger_name, req.body.devoured
   ], function(result) {
     
     res.json({ id: result.insertId });
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,10 +1,9 @@
 $(function() {
     $(".change-devour").on("click", function(event) {
       var id = $(this).data("id");
-      var newDevour = $(this).data("newDevour");
   
       var newDevourState = {
-        devoured: newDevour
+        devoured: true
       };
   
       
@@ -13,7 +12,7 @@ $(function() {
         data: newDevourState
       }).then(
         function() {
-          console.log("changed devour to", newDevour);
+          console.log("changed devour to true");
           
           location.reload();
         }
@@ -25,7 +24,7 @@ $(function() {
       event.preventDefault();
   
       var newBurger = {
-        name: $("#ca").val().trim(),
+        burger_name: $("#ca").val().trim(),
         devoured: $("[name=devoured]:checked").val().trim()
       };
   
